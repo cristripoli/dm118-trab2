4b2b3a36c6154e8941c8b854d94635560da5d82c
Crislaine Tripoli <crislaine@inatel.br>
2016-10-30 21:38:48 -0200

    first commitDDD

---
+++ js/singlePage.js
@@ -0,0 +1,35 @@
+var SinglePage = {
+
+    getInnerHeigh : function () {
+
+        var h = window.innerHeight;
+        /*document.getElementById('home').style.height = h + "px";
+        document.getElementById('portfolio').style.height = h + "px";
+        /*document.getElementById('sobre').style.height = h + "px";*/
+        
+        document.getElementById('home').style.height = document.getElementById('home').style.height/h + "%";
+        document.getElementById('portfolio').style.height = document.getElementById('portfolio').style.height/h + "%";
+        document.getElementById('sobre').style.height = document.getElementById('sobre').style.height/h + "%";
+        
+    },
+
+    onMoveContent : function () {
+
+        var yOffset = window.pageYOffset;
+        var currenHeight = window.innerHeight;
+
+        $(".item-nav").removeClass("currentContent");
+
+        if (yOffset < currenHeight) {
+            $("#nav-home").addClass("currentContent");
+        } else if (yOffset < (2 * currenHeight)) {
+            $("#nav-portfolio").addClass("currentContent");
+        } else if (yOffset >= (2 * currenHeight)) {
+            $("#nav-sobre").addClass("currentContent");
+        }
+    }
+};
+
+window.addEventListener('load',SinglePage.getInnerHeigh);
+window.addEventListener('resize',SinglePage.getInnerHeigh);
+window.addEventListener('scroll', SinglePage.onMoveContent);
